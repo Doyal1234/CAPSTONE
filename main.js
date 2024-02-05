a7d788fd98e78469182e6765a4bddd148bbad03b
Doyal1234 <137032190+Doyal1234@users.noreply.github.com>
2024-02-05 20:54:02 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,74 @@
+var textbox=document.getElementById("Textbox");
+var div1=document.getElementById("div1");
+var div2=document.getElementById("div2");
+var div3=document.getElementById("div3");
+var weight=document.getElementById("weight");
+var height=document.getElementById("height");
+var x= document.getElementById("myAudio");
+function submit() {
+    
+    if(textbox.value>0) {
+        
+        
+       
+        if(textbox.value<=10) {
+            document.getElementById("div1").style.visibility="visible";
+            x.play()
+        }
+        else{
+            document.getElementById("div1").style.visibility="hidden";
+        }
+        if(textbox.value>10&textbox.value<=50) {
+            document.getElementById("div2").style.visibility="visible";
+            x.play()
+        }
+        else{
+            document.getElementById("div2").style.visibility="hidden";
+        }
+        if(textbox.value>50) {
+            document.getElementById("div3").style.visibility="visible";
+            x.play()
+        }
+        else{
+            document.getElementById("div3").style.visibility="hidden";
+        }
+    } 
+   
+        
+        if(height.value<=0){
+            x.pause()
+            document.getElementById("div3").style.visibility="hidden";
+            document.getElementById("div2").style.visibility="hidden";
+            document.getElementById("div1").style.visibility="hidden";
+            document.getElementById("third").style.visibility="visible";
+        }
+
+
+        
+    if(weight.value<=0){
+        x.pause()
+        document.getElementById("div3").style.visibility="hidden";
+        document.getElementById("div2").style.visibility="hidden";
+        document.getElementById("div1").style.visibility="hidden";
+        document.getElementById("second").style.visibility="visible";
+    }
+
+    if(textbox.value<=0){
+        x.pause()
+        document.getElementById("div3").style.visibility="hidden";
+        document.getElementById("div2").style.visibility="hidden";
+        document.getElementById("div1").style.visibility="hidden";
+        document.getElementById("first").style.visibility="visible";
+        
+    }
+    
+}
+function reset() {
+document.getElementById("Textbox").value="";
+document.getElementById("weight").value="";
+document.getElementById("height").value="";
+x.pause()
+document.getElementById("div3").style.visibility="hidden";
+document.getElementById("div2").style.visibility="hidden";
+document.getElementById("div1").style.visibility="hidden";
+}
